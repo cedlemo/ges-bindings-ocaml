c608626ccf6020623929b404481ad9db5cf79149
Manas <manas@likewyss.com>
2018-12-05 13:29:20 +0530

    Initial commit

---
+++ configurator/discover.ml
@@ -0,0 +1,44 @@
+open Base
+open Stdio
+module C = Configurator.V1
+
+let write_sexp fn sexp =
+  Out_channel.write_all fn ~data:(Sexp.to_string sexp)
+
+let () =
+  C.main ~name:"GLib" (fun c ->
+      let default : C.Pkg_config.package_conf =
+        { libs   = ["-lglib-2.0"] ;
+          cflags = ["-O2"; "-Wall"; "-Wextra"; "-Wno-unused-parameter";
+                    "-pthread"; "-I/usr/include/glib-2.0";
+                    "-I/usr/lib/glib-2.0/include"]
+        }
+      in
+      let default_ffi : C.Pkg_config.package_conf =
+        { libs   = ["-lffi"] ;
+          cflags = ["-O2"; "-Wall"; "-Wextra"; "-Wno-unused-parameter";
+                    "-I/usr/lib/libffi-3.2.1/include";
+                    "-I/usr/include/x86_64-linux-gnu"; (* default ubuntu *)
+                    "-I/usr/include"]                  (* default ubuntu *)
+        }
+      in
+      let conf =
+        match C.Pkg_config.get c with
+        | None -> default
+        | Some pc ->
+           let query = C.Pkg_config.query pc ~package:"libffi" in
+           let libffi = Option.value query ~default:default_ffi in
+           let query = C.Pkg_config.query pc ~package:"glib-2.0" in
+           let glib = Option.value query ~default in
+           let  module P = C.Pkg_config in
+           { libs =
+               (libffi.P.libs @ glib.P.libs) @
+                 ["-L/usr/lib/x86_64-linux-gnu";"-L/usr/lib"];
+             cflags =
+               (libffi.P.cflags @ glib.P.cflags) @
+                 ["-I/usr/include"; "-I/usr/include/x86_64-linux-gnu"]
+           }
+      in
+      let list_of_string_to_sexp = sexp_of_list sexp_of_string in
+      write_sexp "c_flags.sexp"         (list_of_string_to_sexp conf.cflags);
+            write_sexp "c_library_flags.sexp" (list_of_string_to_sexp conf.libs))
---
+++ gen/gen.ml
@@ -0,0 +1,40 @@
+module BG = GI_bindings_generator
+module Loader = BG.Loader
+
+(** The namespace to be loaded: ie. the lib for which the bindings will be
+ *  generated. *)
+let namespace = "GES"
+
+let version = "1.0"
+
+(** A suffix for the filenames of the raw bindings of the Core part. For example,
+ *  all the constants and functions defined directly in the namespace are defined
+ *  in a "Core" module and generated in "Core.ml" and "Core.mli" files. But, in
+ *  in order to be able to tweak those automatically generated bindings, a
+ *  suffix is added. Here, all the constants and functions of the namespaces
+ *  will be found in the module Core_raw. Then in the lib/Core.ml file, I just
+ *  load/open the Core_raw. *)
+let files_suffix = "Raw"
+
+(** Instead of generate all the data structures (and theirs related methods or
+ *  constants), the idea is to choose what is needed. *)
+let data_structures =
+  ["Asset";]
+
+(** One can choose to skip the bindings of some constants because they are not
+ *  needed or because you want to create manually the bindings in the "Core.ml"
+ *  file. *)
+let const_to_skip = ["MAJOR_VERSION"; "MINOR_VERSION"; "MICRO_VERSION"]
+
+(** Like for the data_structures, you have to choose with function should have
+ *  its bindings generated. *)
+let functions = ["needs_reload"]
+
+let sources = Loader.generate_files ("Core" ^ files_suffix)
+
+let () =
+  let () = Loader.write_constant_bindings_for namespace ~version sources const_to_skip in
+  let () = Loader.write_function_bindings_for namespace ~version sources functions in
+  let () = Loader.write_enum_and_flag_bindings_for namespace ~version () in
+  let () = Loader.write_bindings_for namespace ~version data_structures in
+  BG.Binding_utils.Sources.close sources
